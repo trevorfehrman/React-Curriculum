6db553a4f591cd423ae47d1705d148677ded6575
Trevor Fehrman <trevorfehrman@gmail.com>
2019-06-26 15:41:19 -0700

    project beta mvp

--- use-state-afternoon-project/src/App.js
+++ use-state-afternoon-project/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import BottomRow from './BottomRow';
 
 function App() {
   return (
@@ -16,24 +17,7 @@ function App() {
             <div className='away__score'>32</div>
           </div>
         </div>
-        <div className='bottomRow'>
-          <div className='down'>
-            <h3 className='down__title'>Down</h3>
-            <div className='down__value'>3</div>
-          </div>
-          <div className='toGo'>
-            <h3 className='toGo__title'>To Go</h3>
-            <div className='toGo__value'>7</div>
-          </div>
-          <div className='ballOn'>
-            <h3 className='ballOn__title'>Ball on</h3>
-            <div className='ballOn__value'>21</div>
-          </div>
-          <div className='quarter'>
-            <h3 className='quarter__title'>3</h3>
-            <div className='quarter__value'>4</div>
-          </div>
-        </div>
+        <BottomRow />
       </section>
       <section className='buttons'>
         <div className='homeButtons'>
